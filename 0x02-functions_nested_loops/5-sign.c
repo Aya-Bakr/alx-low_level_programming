5de972eb37b2e0200b9d18099daff4d67de15d94
Aya-Bakr <ayabakr630@gmail.com>
2023-06-21 14:30:56 -0700

    my task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *Description: is a function  printSign - print + if n <0, zero if =0
+ * - if <0
+ *   @n: integer for function
+ * Return: 1 if +, 0 if 0, -1 if -
+ */
+int printSign(int n)
+	if (n > 0)
+{
+	_putchar(43);
+	return (1);
+}
+else if (n == 0)
+{
+	_putchar(48);
+	return (0);
+}
+else
+{
+	_putchar(45);
+	return (-1);
+}
+}
