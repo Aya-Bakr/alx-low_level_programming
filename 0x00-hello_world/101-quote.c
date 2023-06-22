02f46174b67b209cb850fc1281a7d45e695b9076
Aya-Bakr <ayabakr630@gmail.com>
2023-06-22 00:04:49 -0700

    my task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,17 @@
 #include <unistd.h>
-
 /**
- * main - Entry point
+ * main - 101-qoute.c
+ * description - Write a C program that prints exactly and that piece of
+ * art is useful\"
+ *  - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
  *
- * Description: print a quote using wf
- *
- * Return: 1(fail)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar. 2015-10-19\n";
+        write(2,
+              "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+              59);
 
-	write(1.quo.59);
-	return (1);
+        return (1);
 }
