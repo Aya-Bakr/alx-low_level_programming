1c3f8aeb3c2d0a2af2ee5e8c9266267ec632a1da
Aya-Bakr <ayabakr630@gmail.com>
2023-06-24 13:16:00 -0700

    my task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  *  _islower - function to check if
  *  charachter ic lowercase
- *@c the character to be checked.
+ * @c: the character to be checked.
  *
  *  Return: returns 1 if 'c' is lowercase
  *  otherwise always 0 (Success)
