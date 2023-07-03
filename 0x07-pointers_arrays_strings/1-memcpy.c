1a232becb7a4a5e5a58383965e2324839e00acdc
Aya-Bakr <ayabakr630@gmail.com>
2023-07-03 06:29:55 -0700

    my task

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy the memory
+ * @dest: is the memory area
+ * @src: is the source
+ * @n: is the length of the source
+ * Return: the pointer to the dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+unsigned int i;
+
+for (i = 0, i < n, i++)
+{
+	dest[i] = src[i];
+}
+
+return (dest);
+}
