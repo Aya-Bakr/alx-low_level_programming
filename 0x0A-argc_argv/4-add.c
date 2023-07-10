f9f85dc3a1e6920b109a7c807b0495cf2bd5b644
Aya-Bakr <ayabakr630@gmail.com>
2023-07-10 09:49:01 -0700

    my task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main - a program that adds positive numbers.
+ * @argc: int
+ * @argv: list
+ * Return: always 0
+*/
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *n;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *n, n++)
+			if (*n < '0' || *n > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi argv[argc];
+		printf("%d\n", sum);
+		return (0);
+	}
