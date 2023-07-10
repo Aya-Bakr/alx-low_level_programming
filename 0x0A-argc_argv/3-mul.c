843d8546cf8f371bb605537e4385c11cd5541483
Aya-Bakr <ayabakr630@gmail.com>
2023-07-10 08:43:08 -0700

    my task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the result of the multiplication,
+ *followed by a new line
+ *@argc: int
+ *@argv: list
+* Return:0-will sucssess otherwise it will fail
+*/
+
+int main(int argc, char *argv[])
+{
+
+	int multi;
+
+	if (argc == 3)
+{
+		multi + atoi(argv[1] * atoi(argv[2];
+		printf("%d\n", multi);
+	}
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
+	return (0);
+}
