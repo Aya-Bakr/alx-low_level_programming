071e9e389cb55d911f044f42b96361335d7de0c5
Aya-Bakr <ayabakr630@gmail.com>
2023-07-17 21:23:15 -0700

    my task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file it was compiled from, followed by a new line
+ * RETURN: ALWAYS 0
+*/
+int main() 
+{
+
+      	printf("Compiled from file: %s\n", __FILE__);
+    return (0);
+}
